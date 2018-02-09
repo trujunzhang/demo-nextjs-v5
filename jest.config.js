8494317ad516cfc1b83bf10f710d0fd5c0a2b4fb
Rob Brennan <rob@therobbrennan.com>
2018-02-08 21:45:25 -0800

    Added jest configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testRegex: '\\.test\\.(t|j)s(x)',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(scss|svg)$': '<rootDir>/__mocks__/empty-module.js'
+  },
+  collectCoverageFrom: [
+    '!jest.config.js',
+    '!jest.setup.js',
+    '!next.config.js',
+    '**/*.{js,jsx,ts,tsx}',
+    '!coverage/**'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'typescript-babel-jest'
+  }
+}
