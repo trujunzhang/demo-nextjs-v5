792dd18ad874106fa28d90084bb230f73ce6ee44
Rob Brennan <rob@therobbrennan.com>
2018-02-08 22:39:42 -0800

    Updated server for demo server side rendering

--- server.js
+++ server.js
@@ -10,47 +10,27 @@ app.prepare()
   .then(() => {
     const server = express()
 
-    // server.get('/people', (req, res) => {
-    //   console.log(`
-    //     Express server received GET /people
-    //     \treq.query: ${JSON.stringify(req.query)}
-    //     \treq.params: ${JSON.stringify(req.params)}
-    //   `)
-    //   const actualPage = '/ping'
-    //   const queryParams = { }
-    //   app.render(req, res, actualPage, queryParams)
-    // })
+    server.get('/people', (req, res) => {
+      const actualPage = '/ping'
+      const queryParams = { }
+      app.render(req, res, actualPage, queryParams)
+    })
 
-    // server.get('/people/:slug', (req, res) => {
-    //   console.log(`
-    //     Express server received GET /people/${req.params.slug}
-    //     \treq.query: ${JSON.stringify(req.query)}
-    //     \treq.params: ${JSON.stringify(req.params)}
-    //   `)
-    //   const actualPage = '/ping'
-    //   const queryParams = { slug: req.params.slug, name: req.params.name }
-    //   app.render(req, res, actualPage, queryParams)
-    // })
+    server.get('/people/:slug', (req, res) => {
+      const actualPage = '/ping'
+      const queryParams = { slug: req.params.slug, name: req.params.name }
+      app.render(req, res, actualPage, queryParams)
+    })
 
-    // server.get('/people/:slug/:name', (req, res) => {
-    //   console.log(`
-    //     Express server received GET /people/${req.params.slug}/${req.params.name}
-    //     \treq.query: ${JSON.stringify(req.query)}
-    //     \treq.params: ${JSON.stringify(req.params)}
-    //   `)
-    //   const actualPage = '/ping'
-    //   const queryParams = { slug: req.params.slug, name: req.params.name }
-    //   app.render(req, res, actualPage, queryParams)
-    // })
+    server.get('/people/:slug/:name', (req, res) => {
+      const actualPage = '/ping'
+      const queryParams = { slug: req.params.slug, name: req.params.name }
+      app.render(req, res, actualPage, queryParams)
+    })
 
-    // server.get('/ping', (req, res) => { // SSR for the /ping page in NextJS
-    //   console.log(`
-    //     Express server received GET /ping
-    //     \treq.query: ${JSON.stringify(req.query)}
-    //     \treq.params: ${JSON.stringify(req.params)}
-    //   `)
-    //   return app.render(req, res, '/ping', req.query)
-    // })
+    server.get('/ping', (req, res) => { // SSR for the /ping page in NextJS
+      return app.render(req, res, '/ping', req.query)
+    })
 
     server.get('/test', (req, res) => { // Does not use NextJS app at all
       console.log('Express server solely responsible for processing GET /test')
