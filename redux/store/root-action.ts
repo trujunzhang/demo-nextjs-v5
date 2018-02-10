1e16cf17a2afc0ff3abecadd82621c48984fc61c
Rob Brennan <rob@therobbrennan.com>
2018-02-09 21:59:52 -0800

    Removed unnecessary import

--- redux/store/root-action.ts
+++ redux/store/root-action.ts
@@ -1,5 +1,4 @@
 // Actions
 import { EnthusiasmAction } from '../enthusiasm/actions/index';
-import { MarketsAction } from '../markets/actions/index';
 
-export type RootAction = EnthusiasmAction | MarketsAction;
+export type RootAction = EnthusiasmAction;
