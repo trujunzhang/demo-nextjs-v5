415f8d9f26ded5d85b323a8c2c9eb6d3a8c9649a
Rob Brennan <rob@therobbrennan.com>
2018-02-10 10:29:00 -0800

    Added custom webpack config for bundle analysis

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 const withTs = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
-module.exports = withTs(withSass())
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
+
+module.exports = withTs(withSass({
+  webpack: function (config) {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
+
+    return config
+  }
+}))
