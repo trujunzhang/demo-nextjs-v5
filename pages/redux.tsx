4f51c1cb9d338ebd8e247a2d06f9193f72d78465
Rob Brennan <rob@therobbrennan.com>
2018-02-09 19:03:25 -0800

    Reinstating redux demo page

--- pages/redux.tsx
+++ pages/redux.tsx
@@ -1,51 +1,51 @@
-// import App from '../components/App/App';
-// import * as React from 'react';
-// import * as actions from '../redux/enthusiasm/actions/index';
-// import { RootState } from '../redux/store/root-state';
-// import { Props } from '../redux/enthusiasm/types/enthusiasm';
-// import { Dispatch } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import * as withRedux from 'next-redux-wrapper';
-// import { initStore } from '../redux/store/index';
+import App from '../components/App/App';
+import * as React from 'react';
+import * as actions from '../redux/enthusiasm/actions/index';
+import { RootState } from '../redux/store/root-state';
+import { Props } from '../redux/enthusiasm/types/enthusiasm';
+import { Dispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as withRedux from 'next-redux-wrapper';
+import { initStore } from '../redux/store/index';
 
-// export class Redux extends React.Component<Props> {
-//   render() {
-//     const { enthusiasm, onIncrement, onDecrement } = this.props;
-//     return (
-//       <App>
-//         <div className="myDiv">
-//           <h1>Redux [DEMO]</h1>
-//           <div className="greeting">
-//             Hello {this.getExclamationMarks(enthusiasm.enthusiasmLevel)}
-//           </div>
-//           <div>
-//             <button onClick={onDecrement}>-</button>
-//             <button onClick={onIncrement}>+</button>
-//           </div>
-//           <pre>
-//             {JSON.stringify(this.props)}
-//           </pre>
-//         </div>
-//       </App>
-//     );
-//   }
+export class Redux extends React.Component<Props> {
+  render() {
+    const { enthusiasm, onIncrement, onDecrement } = this.props;
+    return (
+      <App>
+        <div className="myDiv">
+          <h1>Redux [DEMO]</h1>
+          <div className="greeting">
+            Hello {this.getExclamationMarks(enthusiasm.enthusiasmLevel)}
+          </div>
+          <div>
+            <button onClick={onDecrement}>-</button>
+            <button onClick={onIncrement}>+</button>
+          </div>
+          <pre>
+            {JSON.stringify(this.props)}
+          </pre>
+        </div>
+      </App>
+    );
+  }
 
-//   getExclamationMarks(numChars: number) {
-//     return Array(numChars + 1).join('!');
-//   }
-// }
+  getExclamationMarks(numChars: number) {
+    return Array(numChars + 1).join('!');
+  }
+}
 
-// const mapStateToProps = ({ enthusiasm }: RootState) => {
-//   return {
-//     enthusiasm
-//   };
-// };
+const mapStateToProps = ({ enthusiasm }: RootState) => {
+  return {
+    enthusiasm
+  };
+};
 
-// const mapDispatchToProps = (dispatch: Dispatch<actions.EnthusiasmAction>) => {
-//   return {
-//     onIncrement: bindActionCreators(actions.incrementEnthusiasm, dispatch),
-//     onDecrement: bindActionCreators(actions.decrementEnthusiasm, dispatch)
-//   };
-// };
+const mapDispatchToProps = (dispatch: Dispatch<actions.EnthusiasmAction>) => {
+  return {
+    onIncrement: bindActionCreators(actions.incrementEnthusiasm, dispatch),
+    onDecrement: bindActionCreators(actions.decrementEnthusiasm, dispatch)
+  };
+};
 
-// export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Redux);
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Redux);
